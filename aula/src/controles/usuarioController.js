6e5dadcbd83b2e63bf8e1b0b7de078dbf7f5fa0d
DEVair Martins <devair.martins@gmail.com>
2023-03-14 21:21:09 -0300

    buscando email

--- aula/src/controles/usuarioController.js
+++ aula/src/controles/usuarioController.js
@@ -6,12 +6,29 @@ function usuarioController(app){
         res.send(usuarios)
     }
 
+    app.get('/usuario/email/:email', buscar);
+    function buscar(req, res){
+        const usuario = usuarios.find(usuario => usuario.email === req.params.email)
+        if(usuario){
+            res.send(
+                `<h1>Usuário</h1><br/>
+                Nome: ${usuario.nome}<br/>
+                Email: ${usuario.email}<br/>
+                Senha: ${usuario.senha}.`
+            )
+        } else {
+            res.send(`Usuário: ${req.params.email} não encontrado!`);
+        }
+        res.send(req.params.email)
+    }
+
     app.post('/usuario', inserir);
     function inserir(req, res){
         res.send('Inserindo usuário');
         usuarios.push(req.body)
         console.log(req.body)
     }
+
 }
 
 export default usuarioController;
