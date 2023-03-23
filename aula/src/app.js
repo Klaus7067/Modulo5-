51574e7dfea8068e7c11868fa0466ebca0a748b8
DEVair Martins <devair.martins@gmail.com>
2023-03-22 21:34:29 -0300

    falta o cardcomp

--- aula/src/app.js
+++ aula/src/app.js
@@ -2,8 +2,11 @@ import express from 'express'
 import usuarioController from './controles/usuarioController.js'
 import tarefaController from './controles/tarefaController.js'
 import rootController from './controles/rootControler.js';
+import cors from 'cors'
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 
